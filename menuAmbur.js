25da51ce08b7246a13d67d55712f40d1714e1440
Emanuel Lima <lima_lpo@hotmail.com>
2022-08-06 01:15:15 -0300

    retoque

---
+++ menuAmbur.js
@@ -0,0 +1,20 @@
+//menu amburguesa
+
+const menuAmbur=document.getElementById("cont-amburguesa");
+const menuResponsive=document.querySelector(".ul");
+const span1=document.querySelector(".span1");
+const span2=document.querySelector(".span2");
+const span3=document.querySelector(".span3");
+const contAmburguesa=document.querySelector("#cont-amburguesa");
+
+menuAmbur.addEventListener("click", ()=>{
+
+        menuResponsive.classList.toggle("aparece-menu");
+        span1.classList.toggle("span1efecto");    
+        span2.classList.toggle("span2efecto");
+        span3.classList.toggle("span3efecto");
+        contAmburguesa.classList.toggle("fixed"); 
+        span1.classList.toggle("span1efecto2");  
+        
+    
+});
