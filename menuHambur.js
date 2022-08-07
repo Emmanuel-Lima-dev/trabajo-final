4eb31ed7b01675968f43a757b227f079d81c36bc
Emanuel Lima <lima_lpo@hotmail.com>
2022-08-06 21:30:15 -0300

    listo para entregar

--- menuHambur.js
+++ menuHambur.js
@@ -1,20 +1,19 @@
 //menu amburguesa
 
-const menuAmbur=document.getElementById("cont-amburguesa");
+const menuHambur=document.getElementById("cont-hamburguesa");
 const menuResponsive=document.querySelector(".ul");
 const span1=document.querySelector(".span1");
 const span2=document.querySelector(".span2");
 const span3=document.querySelector(".span3");
-const contAmburguesa=document.querySelector("#cont-amburguesa");
 
-menuAmbur.addEventListener("click", ()=>{
+menuHambur.addEventListener("click", ()=>{
 
         menuResponsive.classList.toggle("aparece-menu");
         span1.classList.toggle("span1efecto");
         span1.classList.toggle("segundoSpan1");    
         span2.classList.toggle("span2efecto");
         span3.classList.toggle("span3efecto");
-        contAmburguesa.classList.toggle("fixed");  
+        menuHambur.classList.toggle("fixed");  
         
     
 });
